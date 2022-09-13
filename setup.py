a9b3a4ac4c1b9c7c8bffa93f1680ad935f290894
Hannes Kneiding <hanneskn@uio.no>
2022-09-13 17:54:37 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uxtbpy',
+    version='0.1',
+
+    url='https://github.com/hkneiding/uxtbpy',
+    author='Hannes Kneiding',
+    author_email='hannes.kneiding@outlook.com',
+    packages=find_packages(exclude=['test'])
+)
