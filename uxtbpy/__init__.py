bdb06e2e05075e22584b86b3ea2b7dc6ebdcba98
Hannes Kneiding <k.1996@live.de>
2025-07-23 18:24:15 +0200

    add StdaRunner and StdaOutputParser to exports

--- uxtbpy/__init__.py
+++ uxtbpy/__init__.py
@@ -1,2 +1,4 @@
 from .xtb_runner import XtbRunner  # noqa: F401
 from .xtb_output_parser import XtbOutputParser  # noqa: F401
+from .stda_runner import StdaRunner  # noqa: F401
+from .stda_output_parser import StdaOutputParser  # noqa: F401
