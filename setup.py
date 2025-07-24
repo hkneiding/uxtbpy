0f9b91f63cf3854a97119e83a3f5554fc5d76900
Hannes Kneiding <k.1996@live.de>
2025-07-24 12:14:43 +0200

    update metadata

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="uxtbpy",
-    version="0.1",
+    version="1.0",
+    description="Python interface for executing and parsing results from xTB and sTDA calculations.",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
     url="https://github.com/hkneiding/uxtbpy",
     author="Hannes Kneiding",
     author_email="hannes.kneiding@outlook.com",
+    license="MIT",
+    python_requires=">=3.x",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(exclude=["test"]),
 )
